08728edc445f3490d352c59bdaf99e530da4321f
Fahd AHSAYNI <fahdahsayni@gmail.com>
2025-02-13 21:51:42 +0100

    Fix formatting in number-flow-input component

--- src/components/ui/number-flow-input.tsx
+++ src/components/ui/number-flow-input.tsx
@@ -24,7 +24,7 @@ export function Input({
   const [isEditing, setIsEditing] = React.useState(false);
   const [inputValue, setInputValue] = React.useState(String(value));
 
-  const { openKeyboard } = useVirtualKeyboard(); // <-- grab context methods
+  const { openKeyboard } = useVirtualKeyboard();
 
   React.useEffect(() => {
     if (!isEditing) {
