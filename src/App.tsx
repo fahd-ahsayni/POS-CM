e184a5c0c952adb03345a120de274c05693990f0
Fahd AHSAYNI <fahdahsayni@gmail.com>
2025-03-20 11:43:31 +0000

    refactor: remove KeyboardToggle component and streamline App component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
+import { OrderProvider } from "@/components/global/drawers/order-details/context/OrderContext";
+import OrderDetails from "@/components/global/drawers/order-details/OrderDetails";
 import HandleApp from "./app/HandleApp";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import GlobalVirtualKeyboard from "./components/keyboard/GlobalVirtualKeyboard";
 import { VirtualKeyboardProvider } from "./components/keyboard/VirtualKeyboardGlobalContext";
 import { Toaster } from "./components/ui/toaster";
+import { useLocalStorage } from "./hooks/use-local-storage";
 import NoMobileResponsive from "./page/NoMobileResponsive";
 import ContextsProvider from "./providers/ContextsProvider";
 import Fonts from "./providers/Fonts";
 import { ThemeProvider } from "./providers/themeProvider";
-import { ErrorBoundary } from "./components/ErrorBoundary";
-import { OrderProvider } from "@/components/global/drawers/order-details/context/OrderContext";
-import OrderDetails from "@/components/global/drawers/order-details/OrderDetails";
-import { useLocalStorage } from "./hooks/use-local-storage";
-import KeyboardToggle from "./components/keyboard/KeyboardToggle";
 
 export default function App() {
-  const [withKeyboard, setWithKeyboard] = useLocalStorage<boolean>(
-    "withKeyboard",
-    false
-  );
+  const [withKeyboard] = useLocalStorage<boolean>("withKeyboard", false);
 
   return (
     <ErrorBoundary>
